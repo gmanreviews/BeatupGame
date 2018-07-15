5aee4c9722dfa96463517f2f2ad543661bcc5e9f
gmanreviews <andrew.robinson@gmanreviews.com>
2018-07-15 19:18:55 -0400

    intial commit
    code so far ... just a basic system

---
+++ beatup/Assets/Game/Scripts/Character/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private bool userControlled = false;
+
+    private Animator moveAnim;
+
+    private void UserMovementUpdate()
+    {
+        moveAnim.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+        moveAnim.SetFloat("vertical", Input.GetAxis("Vertical"));
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        this.moveAnim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.userControlled)
+        {
+            this.UserMovementUpdate();
+        }
+
+    }
+}
---
+++ beatup/Assets/Game/Scripts/Utilities/JSONHelper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+// reference: https://answers.unity.com/questions/1123326/jsonutility-array-not-supported.html
+public class JSONHelper
+{
+    public static T[] JSONToArray<T>(string json)
+    {
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+        return wrapper.array;
+    }
+
+    public static string ArraryToJSON<T>(T[] array)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>
+        {
+            array = array
+        };
+
+        return JsonUtility.ToJson(wrapper);
+    }
+
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+        public T[] array;
+    }
+}
