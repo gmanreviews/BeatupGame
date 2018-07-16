bba3cff43a381102442d3e53ccb56df26a0f5e5f
gmanreviews <andrew.robinson@gmanreviews.com>
2018-07-15 22:06:58 -0400

    update to movement
    more movement and character animator done

--- beatup/Assets/Game/Scripts/Character/Movement.cs
+++ beatup/Assets/Game/Scripts/Character/Movement.cs
@@ -3,32 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour
 {
     [SerializeField]
     private bool userControlled = false;
 
     private Animator moveAnim;
+    private Rigidbody2D rigidBdy;
 
     private void UserMovementUpdate()
     {
-        moveAnim.SetFloat("horizontal", Input.GetAxis("Horizontal"));
-        moveAnim.SetFloat("vertical", Input.GetAxis("Vertical"));
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        moveAnim.SetFloat("horizontal", horizontal);
+        moveAnim.SetFloat("vertical", vertical);
+
+        this.rigidBdy.AddForce(new Vector2(horizontal, vertical));
+
     }
 
     // Use this for initialization
     void Start()
     {
         this.moveAnim = this.GetComponent<Animator>();
+        this.rigidBdy = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void FixedUpdate()
+    {
         if (this.userControlled)
         {
             this.UserMovementUpdate();
         }
-
     }
 }
